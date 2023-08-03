f67acd6021da2fd033f8087aa955518ff217e60d
Marisa <marisavandellos@gmail.com>
2023-08-02 18:07:38 -0700

    part 2 done

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ for (i=0; i<arr.length; i++) {
     }
 }
 console.log(oddArray);
+
+//Vowel vs Consonant
+//iterate over a string and count the number of consonants and vowels
+//print the string and the number of consonants and vowels
+let str = "awesome";
+let vowels = 0;
+let consonants = 0;
+for (i=0; i<str.length; i++) {
+    if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+        vowels++;
+    }
+    else {
+        consonants++;
+    }
+}
+console.log(str + " has " + consonants + " consonants and " + vowels + " vowels");
